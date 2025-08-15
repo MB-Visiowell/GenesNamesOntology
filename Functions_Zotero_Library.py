c13c1623b1891a101242d357ccc07508d212c154
MB-Visiowell <morgane.burq@curie.fr>
2025-08-15 16:15:57 +0200

    aaa

--- Functions_Zotero_Library.py
+++ Functions_Zotero_Library.py
@@ -1,1 +1,23 @@
+"""
+Functions for Personal Projects
+Author: Morgane Burq
 
+"""
+import numpy as np
+# import beautifulsoup ?
+
+#########
+
+def Zotero_opening():
+    """
+    Choose the proper Zotero library and screen each paper to find gene names
+
+    Parameters
+    ----------
+    input : name of Zotero library & local access on your computer
+
+    Returns
+    -------
+    result : list of gene names in a CSV that can easily be modified 
+
+    """
